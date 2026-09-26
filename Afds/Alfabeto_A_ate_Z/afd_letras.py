12120fbf1dfc9cde5cfd8356539fb9886b42ddf1
Rafael.Magdaleno <rafaelmagdaleno2@gmail.com>
2026-09-26 14:33:35 -0300

    adiciona AFD da parte [a-zA-Z] do alfabeto da Solis
    Monta, sobre o motor em Afd_Solis/afd.py, o autômato que reconhece
    [a-zA-Z]+ (uma ou mais letras, minúsculas ou maiúsculas). Mesma
    estrutura do AFD de [0-9]: q0 (nenhuma letra lida) e q1 (final, ao
    menos uma letra lida), com a transição q0 -> q1 exigindo a primeira
    letra e o laço em q1 aceitando as seguintes.
    
    Inclui testes para cadeias válidas, cadeia vazia, dígito, caractere
    especial e letra maiúscula.

--- Afds/Alfabeto_A_ate_Z/afd_letras.py
+++ Afds/Alfabeto_A_ate_Z/afd_letras.py
@@ -3,7 +3,6 @@ AFD da parte [a-z] do alfabeto da Solis.
  
 Padrão reconhecido: [a-z]+  (uma ou mais letras minúsculas)
  
-Por enquanto só minúsculas, maiúsculas ficam para depois.
 """
 
 import os
